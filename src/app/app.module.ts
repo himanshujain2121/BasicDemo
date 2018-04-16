8bc92330f1087af219c654199b09c7d85a0e3f27
Himanshu Jain <himanshujain2121@gmail.com>
2018-04-16 23:50:14 +0530

    Added a new employee component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,15 @@
+// Common Module
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+// component
 import { AppComponent } from './app.component';
+import { EmployeeComponent } from './employee/employee.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployeeComponent
   ],
   imports: [
     BrowserModule
