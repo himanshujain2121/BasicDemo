f004cb74832e06c9f05b5aaadf013d0fb3039f87
Himanshu Jain <himanshujain2121@gmail.com>
2018-04-17 12:26:02 +0530

    Added Built-In Directives

--- src/app/employee/employee.component.ts
+++ src/app/employee/employee.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Employee } from './employee';
 
 @Component({
   selector: 'app-employee',
@@ -6,13 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./employee.component.css']
 })
 export class EmployeeComponent implements OnInit {
-  title:string = 'Employee works';
-  visible:boolean = false;
+  title: string = 'Employee works';
+  visible: boolean = false;
+  role:string = "user";
+  employee: Employee[] = [
+    { id: 1, name: 'Himanshu', email: 'himanshu@gmail.com', salary: 60000 },
+    { id: 1, name: 'Namrata', email: 'namrata@gmail.com', salary: 20000 },
+    { id: 1, name: 'Nishu', email: 'nishu@gmail.com', salary: 12000 },
+    { id: 1, name: 'Ankush', email: 'ankush@gmail.com', salary: 37000 }
+  ];
   constructor() { }
 
   ngOnInit() {
   }
-  toggle(){
+  toggle() {
     this.visible = !this.visible;
   }
 
