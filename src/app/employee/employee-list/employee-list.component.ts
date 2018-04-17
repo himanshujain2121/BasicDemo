40fdd4e07b559b17a4f4bbf6cb14579bdb9571cc
Himanshu Jain <himanshujain2121@gmail.com>
2018-04-17 12:53:52 +0530

    @input Component Interaction using HTML

---
+++ src/app/employee/employee-list/employee-list.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Employee } from '../employee';
+
+@Component({
+  selector: 'app-employee-list',
+  templateUrl: './employee-list.component.html',
+  styleUrls: ['./employee-list.component.css']
+})
+export class EmployeeListComponent implements OnInit {
+
+  constructor() { }
+
+  @Input() employeeList:Employee[];
+  ngOnInit() {
+  }
+
+}
