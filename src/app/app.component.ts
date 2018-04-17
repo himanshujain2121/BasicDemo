e4909f5e63533d7ac774cdff2c0112bfad8ab525
Himanshu Jain <himanshujain2121@gmail.com>
2018-04-17 15:52:16 +0530

    added singleton service example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { EmployeeService } from './service/employee/employee.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Basic Demo';
+
+  constructor( private employeeService:EmployeeService){}
+
+  ngOnInit(){
+  }
+  addEmployee(){
+           this.employeeService.addEmployee();
+  }
 }
--- src/app/service/employee/employee.service.ts
+++ src/app/service/employee/employee.service.ts
@@ -8,13 +8,18 @@ export class EmployeeService {
   constructor() { }
 
   getEmployeeList(){
-    this.empList.push(
+    this.empList = [
       { id: 1, name: 'Himanshu', email: 'himanshu@gmail.com', salary: 60000 },
-      { id: 1, name: 'Namrata', email: 'namrata@gmail.com', salary: 20000 },
-      { id: 1, name: 'Nishu', email: 'nishu@gmail.com', salary: 12000 },
-      { id: 1, name: 'Ankush', email: 'ankush@gmail.com', salary: 37000 }
-    );
+      { id: 2, name: 'Namrata', email: 'namrata@gmail.com', salary: 20000 },
+      { id: 3, name: 'Nishu', email: 'nishu@gmail.com', salary: 12000 },
+      { id: 4, name: 'Ankush', email: 'ankush@gmail.com', salary: 37000 }
+  ];
     return this.empList;
+  }
+
+  addEmployee(){
+    let employee =  { id: 5, name: 'Arpit', email: 'arpit@gmail.com', salary: 450000 };
+    this.empList.push(employee);
   }
 
 }
